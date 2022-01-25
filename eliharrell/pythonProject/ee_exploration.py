5af9ece651136664cf27c055da0b05616d199181
eliharrell <eliharrell@uchicago.edu>
2022-01-24 20:39:26 -0600

    made wq tasks

--- eliharrell/pythonProject/ee_exploration.py
+++ eliharrell/pythonProject/ee_exploration.py
@@ -1,9 +1,8 @@
-# This is a sample Python script.
 import ee
 ee.Authenticate()
 ee.Initialize()
 
-landsat = False
+landsat = True
 sentinel_collection = "COPERNICUS/S2_SR"
 landsat_collection = "LANDSAT/S2_SR"
 if landsat:
@@ -19,3 +18,12 @@ else:
     clouds = 'CLOUDY_PIXEL_PERCENTAGE'
 
 clouds = col_sent.filter()
+
+var dataset = ee.Image('AHN/AHN2_05M_RUW');
+var elevation = dataset.select('elevation');
+var elevationVis = {
+  min: -5.0,
+  max: 30.0,
+};
+Map.setCenter(5.76583, 51.855276, 16);
+Map.addLayer(elevation, elevationVis, 'Elevation');
